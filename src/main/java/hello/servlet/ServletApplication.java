7df72b364a40007a59c35219131b7cb2a26ba76d
qkrmekem <magicofclown@naver.com>
2023-03-16 01:36:46 +0900

    request헤더 조회

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,6 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+// 현재 패키지와 하위 패키지에 있는 모든 서블릿을 스캔
+@ServletComponentScan // 서블릿 자동 등록
 
 @SpringBootApplication
 public class ServletApplication {
